5022a03182f2067f073d847217853d74d2610a11
Tai Robinett <tairobinett@gmail.com>
2022-05-26 20:06:55 -0400

    car now accelerates when up/down is held

--- behaviors/race/car.js
+++ behaviors/race/car.js
@@ -2,11 +2,13 @@ class DriveActor {
     setup() {
         if (this.speed === undefined) this.speed = 0.0;
         if (this.angle === undefined) this.angle = 0.0;
+        if (this.accel === undefined) this.accel = 0.0;
         if (this.running === undefined) {
             this.running = true;
             this.run();
         }
         this.addEventListener("keyDown", "control");
+        this.addEventListener("keyUp", "endControl");
         this.addEventListener("pointerDown", "ride");
         this.subscribe(this._cardData.myScope, "newAngle", "newAngle");
         // this.publish(this._cardData.myScope, "reset");
@@ -15,6 +17,13 @@ class DriveActor {
     run() {
         if (!this.running) {return;}
         this.future(20).run();
+        this.speed = this.speed + this.accel; // Add current acceleration to speed
+        if(this.speed > 1.0){ // speed limits
+            this.speed = 1.0;
+        }
+        if(this.speed < -0.2){
+            this.speed = -0.2;
+        }
         this.rotateBy([0, -this.angle, 0]);
         this.forwardBy(-this.speed);
         if (this.avatar) {
@@ -51,18 +60,25 @@ class DriveActor {
             this.angle = Math.min(0.05, this.angle + 0.004);
         } else if (key.key === "ArrowLeft") {
             this.angle = Math.max(-0.05, this.angle - 0.004);
-        } else if (key.key === "ArrowUp") {
-            this.speed = Math.min(1, this.speed + 0.025);
+        } else if (key.key === "ArrowUp") { // up/down arrow keys accelerate the car while held
+            this.accel = 0.01;
         } else if (key.key === "ArrowDown") {
-            this.speed = Math.max(-0.2, this.speed - 0.025);
+            this.accel = -0.01;
         } else if (key.key === "Shift") {
             this.avatar = undefined;
             this.riding = false;
         }
     }
+    endControl(key){
+        if (key.key === "ArrowUp") {
+            this.accel = 0.0;
+        } else if (key.key === "ArrowDown") {
+            this.accel = 0.0;
+        }
+    }
     destroy() {
         this.removeEventListener("pointerDown", "toggle");
-        this.removeEventListener("keyDown", "turn");
+        this.removeEventListener("keyDown", "turn", "keyUp");
         this.running = false;
     }
 }
